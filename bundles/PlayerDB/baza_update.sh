0c2fa1e87bf1b696597d326f2bb3a7c7b8068a14
eld <adam@konczynski.com>
2019-08-07 07:55:01 +0100

    fix

--- bundles/PlayerDB/baza_update.sh
+++ bundles/PlayerDB/baza_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "#CLASS {bazapostaci} OPEN"
-mysql --user=$3 --password=$4 --host=$1 --database=$2 -s -r --execute="select concat('#var postac[',ImieM,'_][gildia] {',Gildia,'};') from bazapostaci union select concat('#var postac[',replace(OpisM,' ', '_'),'_][gildia] {',Gildia,'};') from bazapostaci;" 2>/dev/null|grep -v NULL
-mysql --user=$3 --password=$4 --host=$1 --database=$2 -s -r --execute="select concat('#var postac[',replace(OpisM,' ', '_'),'_][imie] {',ImieM,'};') from bazapostaci;" 2>/dev/null|grep -v NULL
+mysql --user=$3 --password=$4 --host=$1 --database=$2 -s -r --execute="select concat('#var postac[',ImieM,'_][gildia] {',Gildia,'};') from bazapostaci union select concat('#var postac[',replace(OpisM,' ', '_'),'_][gildia] {',Gildia,'};') from bazapostaci;"|grep -v NULL
+mysql --user=$3 --password=$4 --host=$1 --database=$2 -s -r --execute="select concat('#var postac[',replace(OpisM,' ', '_'),'_][imie] {',ImieM,'};') from bazapostaci;"|grep -v NULL
 echo "#CLASS {bazapostaci} CLOSE"
