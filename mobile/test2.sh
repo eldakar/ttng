402f9de84a490d65a9001256212cf83f38dd1006
eldakar <eldakar@users.noreply.github.com>
2020-09-04 19:42:51 +0100

    Create test2.sh

---
+++ mobile/test2.sh
@@ -0,0 +1,3 @@
+echo "extra-keys = [[{key: "F1", display: "ATT"},{key: "F2", display: "⇖"},{key: "F3", display: "⇑"},{key: "F4", display: "⇗"}],[{key: "F5", display: "ZAS"},{key: "F6", display: "⇐"},{key: "F7", display: "QB"},{key: "F8", display: "⇒"}],[{key: "F9", display: "LOK"},{key: "F10", display: "⇙"},{key: "F11", display: "⇓"},{key: "F12", display: "⇘"}]]" > ~/.termux/termux.properties
+
+echo "Dodalem klawisze"
