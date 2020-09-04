619077ecc77447651a8bae1303ebd9920e9124e2
eldakar <eldakar@users.noreply.github.com>
2020-09-04 15:23:42 +0100

    Update test.sh

--- mobile/test.sh
+++ mobile/test.sh
@@ -1,2 +1,3 @@
-echo "extra-keys = [[{key: "F1", display: "CHUJ"},'F2','F3','F4'],['F5','F6','F7','F8'],['F9','F10','F11','F12']]" > ~/.termux/termux.properties
+echo "extra-keys = [[{key: "F1", display: "ATT"},{key: "F2", display: "NW"},{key: "F3", display: "N"},{key: "F4", display: "NE"}],[{key: "F5", display: "ZAS"},{key: "F6", display: "W"},{key: "F7", display: "QB"},{key: "F8", display: "E"}],[{key: "F9", display: "LOK"},{key: "F10", display: "SW"},{key: "F11", display: "S"},{key: "F12", display: "SE"}]]" > ~/.termux/termux.properties
+
 echo "Dodalem klawisze"
