e03ce80e087e3120dcf1a6b44a4a85f25ea08692
eldakar <konczynski@gmail.com>
2021-05-06 11:47:29 +0100

    mobile

---
+++ mob.sh
@@ -0,0 +1,7 @@
+echo !!!! PREDKOSC INSTALACJI ZALEZY OD TELEFONU, OK. 30 minut...  !!!!
+apt update
+apt install tintin++ git nettle gnutls pcre zlib
+
+echo !!!! NASTEPNA KOMENDA ZAJMIE OKOLO 20 MINUT !!!!
+git clone https://github.com/eldakar/ttng.git
+
