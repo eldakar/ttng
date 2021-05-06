c6e614ace7c0395724b2776a7703b58a1600c538
eldakar <konczynski@gmail.com>
2021-05-06 12:13:38 +0100

    mobile

--- mob.sh
+++ mob.sh
@@ -5,3 +5,8 @@ apt install tintin++ git nettle gnutls pcre zlib
 echo !!!! NASTEPNA KOMENDA ZAJMIE OKOLO 20 MINUT !!!!
 git clone https://github.com/eldakar/ttng.git
 
+cd ttng
+sh mobile/add*
+
+echo !!!! ZRESTARTUJ TERMUX !!!!
+
