489108927f249b24d5756d3b9dac9b03eaf9613f
eldakar <eldakar@users.noreply.github.com>
2021-05-17 17:34:34 +0100

    Create azok.sh

---
+++ azok.sh
@@ -0,0 +1,5 @@
+cd
+touch ttng/bundles/ConfigManager/Profiles/azok
+touch ttng/bundles/ConfigManager/Profiles/azok-macros
+touch ttng/bundles/ConfigManager/Profiles/follow-errors
+chmod -R ugo+rwx ttng/bundles/ConfigManager/Profiles/*
