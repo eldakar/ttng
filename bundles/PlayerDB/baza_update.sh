a29b9aa8986565c83670c6af809ca34e7310dc07
eldakar <konczynski@gmail.com>
2021-06-05 17:55:34 +0100

    f

--- bundles/PlayerDB/baza_update.sh
+++ bundles/PlayerDB/baza_update.sh
@@ -6,6 +6,5 @@ mysql --user=$3 --password=$4 --host=$1 --database=$2 -s -r --execute="select co
 echo "#CLASS {bazapostaci} CLOSE"
 
 echo "#CLASS {bossy} OPEN"
-mysql --user=$3 --password=$4 --host=$1 --database=$2 -s -r --execute="select concat('#high {',ImieM,'} {<CAF>};') from bazapostaci where ImieM is not NULL and Gildia = 'BOSS';"|grep -v NULL
 mysql --user=$3 --password=$4 --host=$1 --database=$2 -s -r --execute="select concat('#high {%i',OpisM,'} {<CAF>};') from bazapostaci where ImieM is not NULL and Gildia = 'BOSS';"|grep -v NULL
 echo "#CLASS {bossy} CLOSE"
